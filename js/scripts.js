5393a092c0a61ace070c1aad7e4f5d2a72a4ede2
Chelsea Becker <cschweig2@gmail.com>
2020-10-30 10:50:44 -0700

    PASS Test 3

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,16 @@
 function roboger(strInput) {
   let numDetect = /[0-9]/;
   if (strInput.match(numDetect)) {
-    let numInput = parseInt(strInput);
-    console.log(numInput);
+    console.log("Thank you");
   } else {
-    console.log("Please enter only numbers!");
+    console.log("Please enter only numbers");
+    return;
+  }
+  let numInput = parseInt(strInput);
+  if (numInput > 1 && numInput <= 50) {
+    console.log("go ahead");
+  } else {
+    console.log("Please enter only numbers from 1 to 50");
   }
 }
 
