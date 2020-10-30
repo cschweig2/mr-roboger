8ca8abf3ffab919108c9c1d021ae6a5efd32c40d
Chelsea Becker <cschweig2@gmail.com>
2020-10-30 10:56:52 -0700

    PASS Test 4 and refactor function.

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,20 @@
 function roboger(strInput) {
   let numDetect = /[0-9]/;
   if (strInput.match(numDetect)) {
-    console.log("Thank you");
+    //continue
+    let numInput = parseInt(strInput);
+    if (numInput > 1 && numInput <= 50) {
+      //continue
+      for (let i = 0; i <= numInput; i++) {
+        console.log(i);
+      }
+    } else {
+      console.log("Please enter only numbers from 1 to 50");
+      return;
+    }
   } else {
     console.log("Please enter only numbers");
     return;
-  }
-  let numInput = parseInt(strInput);
-  if (numInput > 1 && numInput <= 50) {
-    console.log("go ahead");
-  } else {
-    console.log("Please enter only numbers from 1 to 50");
   }
 }
 
